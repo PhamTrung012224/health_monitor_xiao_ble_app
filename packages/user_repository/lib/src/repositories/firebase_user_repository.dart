5946897be6f84fc047f89cf9e66de58041c8929a
PhamTrung012224 <trung.pham01152224@gmail.com>
2025-03-31 11:51:26 +0700

    feat: add dart package to handle users

---
+++ packages/user_repository/lib/src/repositories/firebase_user_repository.dart
@@ -0,0 +1,5 @@
+import 'package:user_repository/src/repositories/user_repo.dart';
+
+class FirebaseRepository implements UserRepository{
+
+}
---
+++ packages/user_repository/lib/user_repository.dart
@@ -0,0 +1,5 @@
+library user_repository;
+
+export 'src/models/models.dart';
+export 'src/entities/entities.dart';
+export 'src/repositories/user_repo.dart';
